12dfb5d354bdb6ee7f43fd0cf2bd3a5a2d27f462
FGPlateiro <felipe.plateiro@gmail.com>
2024-11-24 17:38:23 -0300

    atualização dia 23 11 2024

---
+++ semestre_1/PWI_desenvolvimento_web_Davi/Javascript/01_Variaveis_javascript.js
@@ -0,0 +1,55 @@
+/* iniciar esse projeto com base no que foi passado em sala de aula
+e o que o professor disse para consutlarmos a documentação no site da 
+w3schools.com */
+
+// introdução Javascript
+
+// Variaveis 
+
+/* o que são variaveis? são espaços na memorias para armazenar dados
+no javascript podemos declarar variaveis como
+LET, VAR e CONST    
+A palavra var foi usada em javascript de 1995 a 2015
+let e const foram implementadas a partir de 2015
+a boa pratica diz que devemos usar var somente em códigos mais antigos
+*/
+
+/* todas as variaveis em JavaScript devem ser identificadas por nomes exlusivos
+como vamos ver a frente elas podem ser identificadas por uma forma simples como 
+x, y, z ou nomes completos e complexos como idade, nome, soma
+a boa pratica diz para variaveis com mais de uma palavras escrever junto
+com a primeira letra a segunda palavra em maiusculo como
+por exempo "let anoNascimento = 1993", viu que usamos duas palavras juntas
+mas colocando o N de nascimento em maiusculo para facilitar a identificação
+da segunda palavra. */
+
+var exemploVariavel01 = "modo antigo de declarar variavel"
+let exemploVariavel02 = "jeito mais recente de se declarar variaveis, pós 2015"
+const exemploVariavel03 = "variavel do tipo constante, que não se altera o valor"
+
+/* aqui estamos declarando uma variavel do tipo string, string sempre vão estar
+em aspas o Javascript já reconhece  como uma string assm, pois podemos trabalhar
+com numeros em formato de string como no exemplo */
+
+/* 
+Atribuição 
+o sinal de atribuição no Javascript é o "=", assim como em outras linguagens
+esse valor é usado para atribuir valor a uma variavel como usados nos exemplos
+deste arquivo, no arquivo de operadores, vamos falar com mais detalhes.
+*/
+
+let carName = "volvo" 
+const exemploString01 = "50 número como string"
+
+/* Agora vamos trabalhar direto com números 
+por exemplo, o javascript já reconhece o numero sem precisar declarar o seu tipo
+*/
+
+let x, y
+
+x = 5
+y = 10
+
+let soma = x + y
+
+
