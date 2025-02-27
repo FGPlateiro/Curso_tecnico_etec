60a59ccc6a0d0ac839e23767340fdcb359e79e37
unknown <felipe.plateiro@gmail.com>
2025-02-26 22:19:43 -0300

    aula do dia 26/02/2025 do desenvolvimento web 2

--- Semestre_2/Desenvolvimento_web_2/Aula_19_02_2025/Formulario_de_contato/email/contato.php
+++ Semestre_2/Desenvolvimento_web_2/Aula_19_02_2025/Formulario_de_contato/email/contato.php
@@ -7,5 +7,22 @@ $email_destino = array (
      "publicidade" => "publicidade@seusite.com.br",
      "wqebmaster" => "webmaster@seusite.com.br",
 );
+if(isset($_POST['NOME']) && isset($_POST['EMAIL']) && isset($_POST['assunto']) && isset($_POST['message']))
+{
+     $nome = $_POST['nome'];
+     $email = $_POST['email'];
+     $assunto = $_POST['assunto'];
+     $mensagem = $_POST['MENSAGEM'];
+}
+else
+{
+echo "Todos os campos devem ser preenchidos!";
+exit;
+}
+$msg = "nome do usuário: $nome\n";
+$msg .= "E-mail: $small\n";
+$msg .= "mensagem: $mensagem";
+mail($email_destino[assunto], "mensagem do usuário", $msg, "From:contato@seusite.com.br", "-r contato@seusite.com.br");
 
+echo "sua mensagem foi enviada com sucesso!";
 ?>
