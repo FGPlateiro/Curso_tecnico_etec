d98c3076612e451ec92c84b86d29a97e6d2a2858
unknown <felipe.plateiro@gmail.com>
2025-04-09 22:24:46 -0300

    atualização do projeto da aula 26 03 2025

--- Semestre_2/Desenvolvimento_web_2/Aula_26_03_2025/login.php
+++ Semestre_2/Desenvolvimento_web_2/Aula_26_03_2025/login.php
@@ -2,7 +2,7 @@
 // obtem os valores digitados
 $email = $_POST["email"];
 $senha = $_POST["senha"];
-include "conecta_mysqli.inc";
+include "conecta_mysql.inc";
 
 //escapa os caracteres especiais para evitar ataques de SQL Injection
 $email = $conexao -> real_escape_string($email);
@@ -16,14 +16,14 @@ if($linhas==0){
 // teste se a consulta retornou algum registro
 echo "<html><body>";
 echo "<p align=\"center\">E-mail não encontrado!</p>";
-echo "p align=\"center\"><a href=\"login.html\">Voltar</a></p>";
+echo "<p align=\"center\"><a href=\"login.html\">Voltar</a></p>";
 echo "</body></html>";
 } else{
 $dados = $resultado->fetch_array();
 $senha_banco = $dados["senha"];
 if ($senha != $senha_banco){ // confere a senha
 echo "<html><body>";
-echo "<p align=\center\">A senha está incorreta!</p>";
+echo "<p align=\"center\">A senha está incorreta!</p>";
 echo "<p align=\"center\"><a href=\"login.html\">Voltar</a></p>";
 
 echo "</body></html>";
@@ -33,8 +33,8 @@ echo "</body></html>";
 else {
 // usuario e senha corretos. vamos criar os cookies
 
-setcookie("email_usuario", $email);
-setcookie("senha_usuario", $senha);
+setcookie("email_usuario", $email, time() + 3600, "/"); // 1 hora de duração
+setcookie("senha_usuario", $senha, time() + 3600, "/");
 
 // direciona para a pagina inicial dos usuarios cadastrados
 header ("location: pagina_inicial.php");
